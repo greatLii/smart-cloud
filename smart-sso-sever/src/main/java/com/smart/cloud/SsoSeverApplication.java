036e5146f60cfcc59128f58b8d9121ec49dc401b
greatLii <673467415@qq.com>
2021-03-21 21:44:33 +0800

    Initial commit

---
+++ smart-sso-sever/src/main/java/com/smart/cloud/SsoSeverApplication.java
@@ -0,0 +1,13 @@
+package com.smart.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoSeverApplication.class, args);
+    }
+
+}
